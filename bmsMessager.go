eed4bae2a6d7ad3b41400c85e012e9fc4d638a2e
Sharkboy <noreply@shark.boy>
2024-06-22 02:33:46 +0300

    fix

--- bmsMessager.go
+++ bmsMessager.go
@@ -70,7 +70,7 @@ func recCallb(buf []byte) {
 
 		buff = append(buff, buf...)
 		log.Debugf("read end")
-		go parseData()
+		go parseData(buff)
 		buff = nil
 		log.Debugf("release chan")
 		msgWG.Done()
@@ -80,27 +80,27 @@ func recCallb(buf []byte) {
 	}
 }
 
-func parseData() {
-	if mods.IsValid(buff) {
-		log.Debugf("data: %s %d", hex.EncodeToString(buff), len(buff))
-		if buff[1] == 0x03 && len(buff) >= 26 {
+func parseData(data []byte) {
+	if mods.IsValid(data) {
+		log.Debugf("data: %s %d", hex.EncodeToString(data), len(data))
+		if data[1] == 0x03 && len(data) >= 26 {
 
-			mos := mods.GetMOS(buff[24])
-			bmsData.Volts = mods.ToFloat([]byte{buff[4], buff[5]}) / 100
-			bmsData.Current = mods.ToFloat([]byte{buff[6], buff[7]}) / 100
-			bmsData.RemainingCapacity = mods.ToFloat([]byte{buff[8], buff[9]}) / 100
-			bmsData.NominalCapcity = mods.ToFloat([]byte{buff[10], buff[11]}) / 100
-			bmsData.Cycles = mods.ToFloat([]byte{buff[12], buff[13]})
-			bmsData.Version = mods.ToVersion(buff[22])
-			bmsData.RemainingPercent = mods.ToPercents(buff[23])
-			bmsData.Series = mods.ToInt(buff[25])
+			mos := mods.GetMOS(data[24])
+			bmsData.Volts = mods.ToFloat([]byte{data[4], data[5]}) / 100
+			bmsData.Current = mods.ToFloat([]byte{data[6], data[7]}) / 100
+			bmsData.RemainingCapacity = mods.ToFloat([]byte{data[8], data[9]}) / 100
+			bmsData.NominalCapcity = mods.ToFloat([]byte{data[10], data[11]}) / 100
+			bmsData.Cycles = mods.ToFloat([]byte{data[12], data[13]})
+			bmsData.Version = mods.ToVersion(data[22])
+			bmsData.RemainingPercent = mods.ToPercents(data[23])
+			bmsData.Series = mods.ToInt(data[25])
 			//Temp:              toFloat(),
 			bmsData.MosChargingEnabled = mos.Charging
 			bmsData.MosDischargingEnabled = mos.Discharging
 
-			temp := make([]float32, int(buff[26]))
-			for i := 0; i < int(buff[26]); i++ {
-				temperature := (float32(buff[27+i*2])*256 + float32(buff[28+i*2]) - 2731) / 10
+			temp := make([]float32, int(data[26]))
+			for i := 0; i < int(data[26]); i++ {
+				temperature := (float32(data[27+i*2])*256 + float32(data[28+i*2]) - 2731) / 10
 				temp = append(temp, temperature)
 			}
 			bmsData.Temp = temp
